e2222aab4bb5006b9dcd586129864be8e725cbb3
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-07-06 16:06:40 +0300

    Stub and tests for add

--- src/test/java/org/example/PhonebookTest.java
+++ src/test/java/org/example/PhonebookTest.java
@@ -1,7 +1,34 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 
 class PhonebookTest {
+    Phonebook phonebook = new Phonebook();
+
+    @BeforeEach
+    public void createContact() {
+        phonebook.add("Petya", "232323");
+        phonebook.add("Masha", "232323232");
+    }
+
+    @Test
+    public void addTest() {
+        String name = "Petya";
+        String phoneNumber = "32313123";
+        HashMap<String, String> contactListTesT = new HashMap<>();
+        contactListTesT.put("Petya", "89215885937");
+        contactListTesT.put("Masha", "3232323");
+        if (!contactListTesT.containsKey(name)) {
+            contactListTesT.put("Petya", "1231231");
+        }
+        Assertions.assertEquals(contactListTesT.size(), phonebook.add("Petya", "23123123"));
+    }
+
 
 }
+
