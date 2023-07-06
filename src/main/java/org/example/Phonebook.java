416a52fd607c8cd7b5e251c387ba23f2e5cb6e0c
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-07-06 16:08:07 +0300

    add implemented

--- src/main/java/org/example/Phonebook.java
+++ src/main/java/org/example/Phonebook.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Phonebook {
+    HashMap<String,String> contactList = new HashMap<>();
+    public int add(String name,String phonebook){
+        if(!contactList.containsKey(name)){
+            contactList.put(name,phonebook);
+        }
+        return contactList.size();
+    }
 }
