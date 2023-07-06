959c583c1d5bb1ee7354b586173d5a200b7a09d3
Evgeniy Poleshchuk <2x2great@gmail.com>
2023-07-06 18:11:55 +0300

    changes

--- src/test/java/org/example/PhonebookTest.java
+++ src/test/java/org/example/PhonebookTest.java
@@ -11,35 +11,33 @@ import java.util.TreeMap;
 
 class PhonebookTest {
     Phonebook phonebook = new Phonebook();
+    HashMap<String,String> contactList = new HashMap<>();
 
     @BeforeEach
     public void createContact() {
-        phonebook.add("Petya", "232323");
-        phonebook.add("Masha", "232323232");
-
+        phonebook.add("Petya", "123456");
+        phonebook.add("Masha", "654321");
+        phonebook.add("ira", "11223344");
+        phonebook.add("ula", "22113344");
     }
-
-    @Test
-    public void addTest() {
-        String name = "Petya";
-        String phoneNumber = "32313123";
-        HashMap<String, String> contactListTesT = new HashMap<>();
-        contactListTesT.put("Petya", "89215885937");
-        contactListTesT.put("Masha", "3232323");
-        if (!contactListTesT.containsKey(name)) {
-            contactListTesT.put("Petya", "1231231");
-        }
-        Assertions.assertEquals(contactListTesT.size(), phonebook.add("Petya", "23123123"));
-    }
-
-
-    @Test
-    public void findByNumberTest() {
-        HashMap<String, String> contactList = new HashMap<>();
+    @BeforeEach
+    public void putInHash(){
         contactList.put("Petya", "123456");
         contactList.put("Masha", "654321");
         contactList.put("ira", "11223344");
         contactList.put("ula", "22113344");
+    }
+    @Test
+    public void addTest() {
+        String name = "Petya";
+        String phoneNumber = "32313123";
+        if (!contactList.containsKey(name)) {
+            contactList.put("Petya", "1231231");
+        }
+        Assertions.assertEquals(contactList.size(), phonebook.add("Petya", "23123123"));
+    }
+    @Test
+    public void findByNumberTest() {
         String search = String.valueOf(contactList.entrySet()
                 .stream()
                 .filter(user -> user.getValue().equals("11223344"))
@@ -48,14 +46,8 @@ class PhonebookTest {
         phonebook.add("ira", "11223344");
         Assertions.assertEquals(search, phonebook.findByNumber("11223344"));
     }
-
     @Test
     void findByName() {
-        HashMap<String, String> contactList = new HashMap<>();
-        contactList.put("Petya", "123456");
-        contactList.put("Masha", "654321");
-        contactList.put("ira", "11223344");
-        contactList.put("ula", "22113344");
         String search = contactList
                 .keySet().stream()
                 .filter(user -> user.equals("ira"))
@@ -63,21 +55,12 @@ class PhonebookTest {
                 .orElse(null);
         phonebook.add("ira", "11223344");
         Assertions.assertEquals(search,phonebook.findByName("ira"));
-
     }
 
     @Test
     public void printAllNames() {
-        HashMap<String, String> contactList = new HashMap<>();
-        contactList.put("Petya", "123456");
-        contactList.put("Masha", "654321");
-        contactList.put("Bra", "11223344");
-        contactList.put("ula", "22113344");
         TreeMap<String,String> sort = new TreeMap<>(contactList);
         String nameList = sort.keySet().toString();
-        phonebook.add("Bra", "11223344");
-        phonebook.add("ula", "22113344");
-
         Assertions.assertEquals(nameList,phonebook.printAllNames());
     }
 }
